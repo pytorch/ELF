f6f422526f4f67e7a946bdc815f12271598a5904
Jerry Ma <noreplyspamblackhole@gmail.com>
2018-07-26 11:17:36 -0700

    Cleanup and bugfixing for scripts

--- scripts/elfgames/go/train.py
+++ scripts/elfgames/go/train.py
@@ -8,14 +8,24 @@
 # LICENSE file in the root directory of this source tree.
 
 import os
+import sys
 import re
 import time
 
+import torch
+
 from rlpytorch import load_env, SingleProcessRun, Trainer
+
 
 matcher = re.compile(r"save-(\d+).bin")
 
-if __name__ == '__main__':
+
+def main():
+    print('Python version:', sys.version)
+    print('PyTorch version:', torch.__version__)
+    print('CUDA version', torch.version.cuda)
+    print('Conda env:', os.environ.get("CONDA_DEFAULT_ENV", ""))
+
     additional_to_load = {
         'trainer': (
             Trainer.get_option_spec(),
@@ -48,17 +58,16 @@ if __name__ == '__main__':
     eval_old_model = env["game"].options.eval_old_model
 
     if eval_old_model >= 0:
-        GC.GC.setEvalMode(model_ver, eval_old_model)
+        GC.GC.getServer().setEvalMode(model_ver, eval_old_model)
     else:
-        GC.GC.setInitialVersion(model_ver)
+        GC.GC.getServer().setInitialVersion(model_ver)
 
     selfplay_ver = model_ver
     root = os.environ["save"]
-    print("Root: " + root)
-    print("Keep prev_selfplay: " + str(keep_prev_selfplay))
+    print(f'Root: "{root}"')
+    print(f'Keep prev_selfplay: {keep_prev_selfplay!s}')
 
     def train(batch, *args, **kwargs):
-        global trainer, selfplay_ver, keep_prev_selfplay, runner
         # Check whether the version match.
         if keep_prev_selfplay or \
                 (batch["selfplay_ver"] != selfplay_ver).sum() == 0:
@@ -69,12 +78,12 @@ if __name__ == '__main__':
             runner.inc_episode_counter(-1)
 
     def train_ctrl(batch, *args, **kwargs):
-        global selfplay_ver, env, model_loader, GC, root, trainer
+        nonlocal selfplay_ver
         old_selfplay_ver = selfplay_ver
         selfplay_ver = int(batch["selfplay_ver"][0])
         print(
             f'Train ctrl: selfplay_ver: {old_selfplay_ver} -> {selfplay_ver}')
-        GC.GC.waitForSufficientSelfplay(selfplay_ver)
+        GC.GC.getServer().waitForSufficientSelfplay(selfplay_ver)
 
         # Reload old models.
         real_path = os.path.join(root, "save-" + str(selfplay_ver) + ".bin")
@@ -111,21 +120,25 @@ if __name__ == '__main__':
         rl_method=env["method"])
 
     def episode_summary(i):
-        global GC, selfplay_ver
+        nonlocal selfplay_ver
         ver = trainer.episode_summary(i)
         # This might block (when evaluation does not catch up with training).
-        GC.GC.notifyNewVersion(selfplay_ver, ver)
+        GC.GC.getServer().notifyNewVersion(selfplay_ver, ver)
 
     offline_training = (env["game"].options.mode == "offline_train")
 
     def after_start():
-        global selfplay_ver, offline_training
+        nonlocal selfplay_ver
         if not offline_training:
             print("About to wait for sufficient selfplay")
-            GC.GC.waitForSufficientSelfplay(selfplay_ver)
+            GC.GC.getServer().waitForSufficientSelfplay(selfplay_ver)
 
     runner.setup(GC, after_start=after_start,
                  episode_summary=episode_summary,
                  episode_start=trainer.episode_start)
 
     runner.run()
+
+
+if __name__ == '__main__':
+    main()
