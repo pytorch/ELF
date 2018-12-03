d88db7031a77a3d0bd53f831867e052e4b8ca380
Tristan Cazenave <Tristan.Cazenave@gmail.com>
2018-12-03 14:52:05 +0100

    version intiale de breakthrough

--- edit.sh
+++ edit.sh
@@ -5,12 +5,12 @@ ROOTDIR=`git rev-parse --show-toplevel`
 for i in `find ${ROOTDIR}/ -iname $1 | grep "\/task"`
 do
 echo $i
-vim $i
+emacs $i
 done
 # Find a file with name $1 and edit it (second pass)
 for i in `find ${ROOTDIR}/ -iname $1 | grep -v "\/task"`
 do
 echo $i
-vim $i
+emacs $i
 done
 #find ~/newtasks -iname $1 
