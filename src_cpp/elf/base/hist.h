5c3ef94d5642370100d1ae07afc04be91e47b54b
Yuandong Tian <yuandong.tian@gmail.com>
2018-12-14 21:31:56 -0800

    Some changes in hist.h

--- src_cpp/elf/base/hist.h
+++ src_cpp/elf/base/hist.h
@@ -10,66 +10,113 @@
 
 #include <cassert>
 #include <vector>
+#include <functional>
 
 namespace elf {
+
+template <typename T>
+class HistTrait;
+
+template <typename T>
+class HistTrait<std::vector<T>> {
+ public:
+  HistTrait(int vec_size, T undef_value = 0) 
+    : vec_size_(vec_size), undef_value_(undef_value) {
+  }
+
+  void Initialize(std::vector<T> &v) const {
+    v.resize(vec_size_);
+    std::fill(v.begin(), v.end(), undef_value_);
+  } 
+
+  size_t Extract(const std::vector<T> &v, T *s) const {
+    copy(v.begin(), v.end(), s);
+    return v.size();
+  }
+
+ private:
+  int vec_size_;
+  T undef_value_;
+};
 
 // Accumulate history buffer.
 template <typename T>
 class HistT {
  public:
-  HistT(size_t q_size, size_t vec_size, T undef_val = 0)
-      : vec_size_(vec_size), undef_val_(undef_val) {
-    q_ = std::vector<std::vector<T>>(q_size, std::vector<T>(vec_size_));
-    assert(q_.size() > 0);
-    assert(q_[0].size() == vec_size_);
+  using Initializer = std::function<void (T &)>;
+
+  HistT(size_t q_size) {
+    q_ = std::vector<T>(q_size);
   }
 
-  void reset() {
-    for (auto &v : q_) {
-      std::fill(v.begin(), v.end(), undef_val_);
+  void reset(Initializer initializer) {
+    if (initializer != nullptr) {
+      for (auto &v : q_) {
+        initializer(v);
+      }
     }
   }
 
-  T* prepare() {
+  size_t size() const { return q_.size(); }
+
+  T &push(T &&v) {
     // q_idx_ always points to the most recent entry.
     q_idx_ = (q_idx_ + 1) % q_.size();
-    return &q_[q_idx_][0];
+    q_[q_idx_] = std::move(v);
+    return q_[q_idx_];
   }
 
-  void extract(T* s) const {
+  size_t accumulate(std::function<size_t (const T &)> func) const {
+    size_t sz = 0;
+    for (const T &v : q_) {
+      sz += func(v);
+    }
+    return sz;
+  }
+
+  template <typename S>
+  void extract(S* s, size_t (T::*extractor)(S *) const) const {
+    auto f = [=](const T &v, S *s) {
+      return (v.*extractor)(s);
+    };
+    this->template extract<S>(s, f);
+  }
+
+  template <typename S>
+  void extract(S* s, std::function<size_t (const T &, S *)> extractor) const {
+    assert(extractor != nullptr);
     // one sample = dim per feature * time length
     size_t idx = q_idx_;
     for (size_t i = 0; i < q_.size(); ++i) {
-      const auto &v = q_[idx];
-      assert(v.size() == vec_size_);
-      copy(v.begin(), v.end(), s);
-      s += v.size();
+      const T &v = q_[idx];
+      s += extractor(v, s);
 
       if (idx == 0) idx = q_.size() - 1;
       else idx --;
     }
   } 
 
-  void extractHistBatch(T* s, int batchsize, int batch_idx) const {
-    T* start = s + batch_idx * vec_size_;
+  /* 
+  void extractHistBatch(S* s, int batchsize, int batch_idx, Extractor extractor) const {
+    assert(extractor != nullptr);
+
+    S* start = s + batch_idx * vec_size_;
     int stride = batchsize * vec_size_;
     size_t idx = q_idx_;
     for (size_t i = 0; i < q_.size(); ++i) {
-      const auto &v = q_[idx];
-      assert(v.size() == vec_size_);
-      copy(v.begin(), v.end(), s);
+      const T &v = q_[idx];
+      extractor(v, s);
       start += stride;
 
       if (idx == 0) idx = q_.size() - 1;
       else idx --;
     }
   }
+  */
 
  private:
-  std::vector<std::vector<T>> q_;
+  std::vector<T> q_;
   size_t q_idx_ = 0;
-  size_t vec_size_;
-  T undef_val_;
 };
 
 } // namespace elf
