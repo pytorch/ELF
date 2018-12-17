9b17799428816ca9e6a54599e6dc49b8d381b2b5
Yuandong Tian <yuandong.tian@gmail.com>
2018-12-16 19:32:51 -0800

    Fix bug

--- src_py/rl/methods/actor_critic.py
+++ src_py/rl/methods/actor_critic.py
@@ -10,6 +10,7 @@ from .discounted_reward import DiscountedReward
 from .value_matcher import ValueMatcher
 from .utils import add_err
 
+import torch
 
 class ActorCritic(object):
     """An actor critic model."""
