d5a2f23e884e24e4755991e19799627d49637f96
Hengyuan Hu <hengyuan@fb.com>
2019-02-16 22:47:00 -0800

    reorg/remove src_oy

--- src_py/rlpytorch/trainer/__init__.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2018-present, Facebook, Inc.
-# All rights reserved.
-#
-# This source code is licensed under the BSD-style license found in the
-# LICENSE file in the root directory of this source tree.
-
-from .utils import ModelSaver, MultiCounter, topk_accuracy
-from .trainer import Trainer, Evaluator
-from .lstm_trainer import LSTMTrainer
