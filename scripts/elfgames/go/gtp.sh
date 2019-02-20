49840baa1b012c2831309a45cd7f8d50c1137490
Jerry Ma <noreplyspamblackhole@gmail.com>
2019-02-20 14:44:20 -0800

    GTP.SH replace prefix fix

--- scripts/elfgames/go/gtp.sh
+++ scripts/elfgames/go/gtp.sh
@@ -12,7 +12,7 @@ shift
 game=elfgames.go.game model=df_pred model_file=elfgames.go.df_model3 python3 df_console.py --mode online --keys_in_reply V rv \
     --use_mcts --mcts_verbose_time --mcts_use_prior --mcts_persistent_tree --load $MODEL \
     --server_addr localhost --port 1234 \
-    --replace_prefix resnet.module,resnet \
+    --replace_prefix resnet.module,resnet init_conv.module,init_conv \
     --no_check_loaded_options \
     --no_parameter_print \
     "$@"
